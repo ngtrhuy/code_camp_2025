8306cfc9f13b57b5b1ab30da66942f5dae1cd7f3
Nguyenx anh ducs <135204041+nguyenanhduc27@users.noreply.github.com>
2025-07-28 11:43:42 +0700

    import vao db

--- TouristApp/Program.cs
+++ TouristApp/Program.cs
@@ -1,50 +1,64 @@
-
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 using TouristApp.Services;
 using TouristApp.Services.LuaViet;
+using TouristApp.Data;
 
-namespace TouristApp
+var builder = WebApplication.CreateBuilder(args);
+
+// 1. Đăng ký DbContext với MySQL
+builder.Services.AddDbContext<TourDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("MySql"),
+        new MySqlServerVersion(new Version(8, 0, 30))
+    )
+);
+
+// 2. Đăng ký các service
+builder.Services.AddScoped<TourScraperService>();
+builder.Services.AddScoped<TourSeleniumService>();
+builder.Services.AddScoped<TourImportService>();
+builder.Services.AddHttpClient<PystravelCrawlService>();
+builder.Services.AddHttpClient<LuaVietTourCrawler>();
+
+// 3. Controller & Swagger
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// 4. CORS
+builder.Services.AddCors(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
+    options.AddPolicy("AllowAll",
+        policy =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+            policy.AllowAnyOrigin()
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
 
-            // Add services to the container.
+var app = builder.Build();
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddHttpClient<PystravelCrawlService>();
-            builder.Services.AddHttpClient<LuaVietTourCrawler>();
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy("AllowAll",
-                    policy =>
-                    {
-                        policy.AllowAnyOrigin()
-                              .AllowAnyMethod()
-                              .AllowAnyHeader();
-                    });
-            });
+// 5. Middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-            app.UseHttpsRedirection();
-            app.UseCors("AllowAll");
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
+    // Áp dụng migration DB (tùy chọn, chỉ trong dev)
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbContext = scope.ServiceProvider.GetRequiredService<TourDbContext>();
+        dbContext.Database.Migrate();
     }
 }
+
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
